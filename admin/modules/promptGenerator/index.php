9a8e8e2926badd3fa39aa24d970292c1e5425c96
Charles Bishop <charleshb417@gmail.com>
2021-01-09 16:06:11 -0500

    Basic prompt creation form with error handling

--- admin/modules/promptGenerator/index.php
+++ admin/modules/promptGenerator/index.php
@@ -14,18 +14,74 @@ if(!defined("IN_MYBB"))
 	die("Direct initialization of this file is not allowed.<br /><br />Please make sure IN_MYBB is defined.");
 }
 
-if(!$mybb->input['action'])
+$page->add_breadcrumb_item($lang->promptGenerator_breadcrumb);
+$page->output_header($lang->promptGenerator_breadcrumb);
+
+$sub_tabs = array();
+$sub_tabs['promptGenerator'] = array(
+	'title' => $lang->promptGenerator_breadcrumb,
+	'link' => "index.php?module=promptGenerator",
+	'description' => $lang->setting_group_promptGenerator_desc
+);
+
+$sub_tabs['add_forum'] = array(
+		'title' => $lang->promptGenerator_add,
+		'link' => "index.php?module=promptGenerator&amp;action=add",
+		'description' => $lang->add_forum_desc
+);
+
+$page->output_nav_tabs($sub_tabs, 'edit_mod');
+
+if ( $mybb->input['action'] == 'add')
 {
-	$page->add_breadcrumb_item($lang->promptGenerator_breadcrumb);
-	$page->output_header($lang->promptGenerator_breadcrumb);
+	if($mybb->request_method == "post")
+	{
+		if(!trim($mybb->input['prompt']))
+		{
+			$errors[] = $lang->promptGenerator_error_missing_title;
+		}
+		else
+		{
+			// trim excess whitespace
+			$p = trim($mybb->input['prompt']);
 
-	$sub_tabs['promptGenerator'] = array(
-		'title' => $lang->promptGenerator_breadcrumb,
-		'link' => "index.php?module=promptGenerator",
-		'description' => $lang->setting_group_promptGenerator_desc
-	);
+			// replace newlines with spaces
+			$p = trim(preg_replace('/\s\s+/', ' ', $p));
 
+			if (strlen($p) > 2048){
+				$errors[] = $lang->promptGenerator_error_too_long;
+			}
+		}
 
+		if (!$errors)
+		{
+			$insert_array = array(
+				"prompt" => $db->escape_string($p)
+			);
+
+			$pid = $db->insert_query("prompt_generator", $insert_array);
+			flash_message($lang->promptGenerator_add_success, 'success');
+			admin_redirect("index.php?module=promptGenerator");
+		}
+		else
+		{
+			$page->output_inline_error($errors);
+		}
+
+	}
+
+	$form = new Form("index.php?module=promptGenerator&amp;action=add", "post");
+	$form_container = new FormContainer($lang->promptGenerator_add);
+	$form_container->output_row($lang->promptGenerator_header_prompt, "", $form->generate_text_area('prompt', '', array('id' => 'prompt', 'maxlength'=>2048, 'required'=>true)), 'prompt');
+	$form_container->end();
+
+	$buttons[] = $form->generate_submit_button($lang->promptGenerator_submit_button);
+	$form->output_submit_wrapper($buttons);
+	$form->end();
+
+}
+elseif(!$mybb->input['action'])
+{
 	$table = new Table;
 	$table->construct_header($lang->promptGenerator_header_prompt, array("colspan" => 1));
 
@@ -45,5 +101,6 @@ if(!$mybb->input['action'])
 
 	$table->output($lang->dashboard);
 
-	$page->output_footer();
 }
+
+$page->output_footer();
--- inc/languages/english/admin/promptGenerator_module_meta.lang.php
+++ inc/languages/english/admin/promptGenerator_module_meta.lang.php
@@ -2,7 +2,15 @@
 
 $l['setting_group_promptGenerator'] = 'Prompt Generator';
 $l['setting_group_promptGenerator_desc'] = 'Settings for Prompt Generator Plugin.';
+$l['promptGenerator_add'] = 'Add Prompt';
+
+$l['promptGenerator_submit_button'] = 'Submit Prompt';
 
 $l['promptGenerator_header_prompt'] = 'Prompt';
 
 $l['promptGenerator_breadcrumb'] = 'Prompt Generator Config';
+
+$l['promptGenerator_error_missing_title'] = 'Oh no! The prompt is missing!';
+$l['promptGenerator_error_too_long'] = 'Your prompt must be fewer than 2048 characters.';
+
+$l['promptGenerator_add_success'] = 'Prompt was successfully added.';
